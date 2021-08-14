954c865af6fb00f581d1e6279323113eb1d69674
krishna9304 <krishhtrishh9304@gmail.com>
2021-08-14 13:19:15 +0530

    login and signup page ui

---
+++ client/src/pages/LogIn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "../components/loginform";
+
+const LoginPage = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUpForm from "../components/signupform";
+
+const SignUpPage = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignUpPage;
