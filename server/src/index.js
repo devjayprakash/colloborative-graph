a75be4ec8e7fa0fe7d31e83b3e4d4caddf1b1734
krishna9304 <krishhtrishh9304@gmail.com>
2021-08-14 13:48:10 +0530

    gg

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,20 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import morgan from 'morgan'
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
-let app = express()
+let app = express();
 
 //setting up the env file
-dotenv.config()
+dotenv.config();
 
 //ALL THE MIDDLEWARES
-app.use(cors()) //TODO : Set it to only our frontend
-app.use(helmet())
-app.use(morgan('tiny'))
+app.use(cors()); //TODO : Set it to only our frontend
+app.use(helmet());
+app.use(morgan("tiny"));
 
-let PORT = process.env.PORT || 8080
-app.listen(PORT , () => {
-    console.log("Server started on port %d" , PORT);
-})
+let PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log("Server started on port %d", PORT);
+});
